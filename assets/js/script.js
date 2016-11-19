aa57d95a58ed3c5bc552f4c80e1e17f3f4e3916a
Yakiel <codey4k@gmail.com>
2016-11-19 00:45:00 +0000

    Añadiendo responsive design e incluyendo jquery

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+$(window).on('scroll',()=>{
+	
+	let s = $(window).scrollTop(),
+		 header = $('.nav');
+	
+	if(s >= 70){
+		header.css({'background':'rgba(240,240,240,.5)',
+		            'height':'60px',
+		            'box-shadow' : '0 5px 20px rgba(0,0,0,.2)'
+	    });
+	}
+	else{
+		header.css({'background' : 'transparent',
+		            'height':'80px',
+		            'box-shadow' : 'none'
+	    });
+    }
+});
+
+$('.fab').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 200 ){
+			$('.fab').slideDown(300);
+		} else {
+			$('.fab').slideUp(300);
+		}
+	});
