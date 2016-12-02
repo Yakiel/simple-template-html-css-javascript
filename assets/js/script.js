c65cf1da769cb6b080d74b40b1dc012e09f0c453
Yakiel <codey4k@gmail.com>
2016-12-02 22:31:36 +0000

    Code update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $(window).on('scroll',()=>{
 		 header = $('.nav');
 	
 	if(s >= 70){
-		header.css({'background':'rgba(240,240,240,.5)',
+		header.css({'background':'rgba(240,240,240,.6)',
 		            'height':'60px',
 		            'box-shadow' : '0 5px 20px rgba(0,0,0,.2)'
 	    });
@@ -17,16 +17,18 @@ $(window).on('scroll',()=>{
     }
 });
 
+
 $('.fab').click(function(){
 		$('body, html').animate({
 			scrollTop: '0px'
 		}, 300);
 	});
  
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 200 ){
-			$('.fab').slideDown(300);
-		} else {
-			$('.fab').slideUp(300);
-		}
-	});
+$(window).scroll(function(){
+	if( $(this).scrollTop() > 200 ){
+		$('.fab').slideDown(300);
+	} 
+	else {
+		$('.fab').slideUp(300);
+	}
+});
